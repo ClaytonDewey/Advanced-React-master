65255537af25f5c404ee60992831ec372c1ff81e
ClaytonDewey <clay@dryadmedia.com>
2022-12-29 15:34:22 -0600

    Map and display products

---
+++ sick-fits/frontend/components/Product.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import formatMoney from '../lib/formatMoney';
+import ItemStyles from './styles/ItemStyles';
+import Title from './styles/Title';
+import PriceTag from './styles/PriceTag';
+
+const Product = ({ product }) => (
+  <ItemStyles>
+    <img src={product?.photo?.image?.publicUrlTransformed} alt={product.name} />
+    <Title>
+      <Link href={`/product/${product.id}`}>{product.name}</Link>
+    </Title>
+    <PriceTag>{formatMoney(product.price)}</PriceTag>
+    <p>{product.description}</p>
+    {/* TODO: Add buttons to edit and delete item */}
+  </ItemStyles>
+);
+
+export default Product;
