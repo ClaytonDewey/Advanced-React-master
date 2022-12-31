6501058f7a0fe6a057143d410188976cacb1affe
Clayton Dewey <clay@dryadmedia.com>
2022-12-31 16:24:43 -0600

    Setup signin/authentication

---
+++ sick-fits/frontend/pages/signin.js
@@ -0,0 +1,12 @@
+import SignIn from '../components/SignIn';
+
+const SignInPage = () => {
+  console.log('Something');
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInPage;
