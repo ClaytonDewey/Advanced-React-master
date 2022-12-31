04277925dce9b72956a2797febd5857cfed44255
Clayton Dewey <clay@dryadmedia.com>
2022-12-31 14:17:02 -0600

    Remove console.log statements

--- sick-fits/frontend/lib/paginationField.js
+++ sick-fits/frontend/lib/paginationField.js
@@ -3,7 +3,6 @@ import { PAGINATION_QUERY } from '../components/Pagination';
 const paginationField = () => ({
   keyArgs: false, // tells apollo we will take care of everything
   read(existing = [], { args, cache }) {
-    console.log({ existing, args, cache });
     const { skip, first } = args;
 
     // Read the number of items on the page from the cache
@@ -29,9 +28,6 @@ const paginationField = () => ({
 
     // If there are items, just return them from the cache, and we don't need to go to the network
     if (items.length) {
-      console.log(
-        `There are ${items.length} items in the cache! Gonna send them to apollo`
-      );
       return items;
     }
 
@@ -44,12 +40,10 @@ const paginationField = () => ({
   merge(existing, incoming, { args }) {
     const { skip, first } = args;
     // This runs when the Apollo comes back from the network with our products
-    console.log(`Merging items from the network ${incoming.length}`);
     const merged = existing ? existing.slice(0) : [];
     for (let i = skip; i < skip + incoming.length; ++i) {
       merged[i] = incoming[i - skip];
     }
-    console.log(merged);
     // Finally we return the merged items from the cache
     return merged;
   },
