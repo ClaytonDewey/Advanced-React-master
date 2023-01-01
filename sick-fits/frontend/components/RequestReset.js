5d4f0c9741c0f970463a870e889b8617bd3cea26
Clayton Dewey <clay@dryadmedia.com>
2023-01-01 15:08:52 -0600

    Fix typo in filename

---
+++ sick-fits/frontend/components/RequestReset.js
@@ -0,0 +1,72 @@
+/* eslint-disable react/jsx-no-bind */
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+import Form from './styles/Form';
+import useForm from '../lib/useForm';
+// import { CURRENT_USER_QUERY } from './User';
+import DisplayError from './ErrorMessage';
+
+const REQUEST_RESET_MUTATION = gql`
+  mutation REQUEST_RESET_MUTATION($email: String!) {
+    sendUserPasswordResetLink(email: $email) {
+      code
+      message
+    }
+  }
+`;
+
+const RequestReset = () => {
+  const { inputs, handleChange, resetForm } = useForm({
+    email: '',
+    name: '',
+    password: '',
+  });
+  const [signup, { data, loading, error }] = useMutation(
+    REQUEST_RESET_MUTATION,
+    {
+      variables: inputs,
+      // refetch the currently logged in user
+      // refetchQueries: [{ query: CURRENT_USER_QUERY }],
+    }
+  );
+  async function handleSubmit(e) {
+    e.preventDefault(); // stop the form from submitting
+    console.log(inputs);
+    const res = await signup().catch(console.error(error));
+    console.log(res);
+    console.log({ data, loading, error });
+    resetForm();
+    // Send the email and password to the graphqlAPI
+  }
+  // const error =
+  //   data?.authenticateUserWithPassword.__typename ===
+  //   'UserAuthenticationWithPasswordFailure'
+  //     ? data?.authenticateUserWithPassword
+  //     : undefined;
+  return (
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>Forgot Password?</h2>
+      <DisplayError error={error} />
+      <fieldset>
+        {data?.sendUserPasswordResetLink === null && (
+          <p>Success! Check your email for a link</p>
+        )}
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Your Email Address"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Request Reset</button>
+      </fieldset>
+    </Form>
+  );
+};
+
+export default RequestReset;
